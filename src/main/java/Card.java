34b5dd52b7ea04a5d6a3b1978b921b8a6ca02b3d
yaseman-nikoo-ics4u-03-25 <yaseman.nikoo@student.tdsb.on.ca>
2025-05-06 18:38:52 +0000

    modify

---
+++ src/main/java/Card.java
@@ -0,0 +1,20 @@
+public class Card{
+    private String name;
+    private String suit;
+    private int value;
+    public Card(String name, String suit, int value){
+        this.name=name;
+        this.suit=suit;
+        this.value=vlue;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public String getSuit(){
+        return this.suit;
+    }
+    public int getValue(){
+        return this.value;
+    }
+    public String 
+}
